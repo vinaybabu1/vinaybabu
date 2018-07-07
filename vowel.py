4357dd4a707b88aa0a08a6b7a5a73e65c8936479
vinaybabu1 <40800496+vinaybabu1@users.noreply.github.com>
2018-07-07 11:34:03 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+n=raw_input()
+if(n="a" or n="e" or n="i" or n="o" or n="u"):
+   print("vowel")
+elif(n="b" or n="c" or n="d" or n="f" or n="g" or n="h" or n="j" or n="k" or n="l" or n="m" n="n" or n="p" or n="q" or n="s" or n="t" or n="v" or n="w" or n="x" or n="y" or n="z"):
+   print("constant")
+ else:
+   print("invalid")
