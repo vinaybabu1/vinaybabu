a7a00bb4f28f89523448424080ed33e98de46250
vinaybabu1 <40800496+vinaybabu1@users.noreply.github.com>
2018-07-07 09:58:29 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,6 +1,6 @@
-n=int(input("enter a number:"))
+n=int(raw_input())
 count=0
-while (n>0):
+while(n>0):
     count=count+1
     n=n//10
-print("the number of digits in a number are",)    
+print(count)
