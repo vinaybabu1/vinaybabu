5922b90bad87bcb6d973563f5be898dff1444292
vinaybabu1 <40800496+vinaybabu1@users.noreply.github.com>
2018-07-07 09:38:09 +0530

    Update hnm.py

--- hnm.py
+++ hnm.py
@@ -1,6 +1,6 @@
 num1=float(input("enter first number:"))
 num2=float(input("enter second number:"))
-num3=float(input("enter third number:))
+num3=float(input("enter third number":))
 if (num1>num2)and(num1>num3):
                  largest=num1
 elif (num2>num1)and(num2>num3):
