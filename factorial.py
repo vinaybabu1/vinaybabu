c1d6b86c5110c2d46d6bd1ca90818d416ef66c74
vinaybabu1 <40800496+vinaybabu1@users.noreply.github.com>
2018-07-12 11:35:40 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,8 +3,8 @@ factorial=1
 if num < 0:
   print("sorry, factorial does npot exists for negative number")
 elif num == 0:
-  print("the facorial of 0 is 1")
+  print("the factorial of 0 is 1")
 else:
   for i in range(1,num+1):
     factorial=factorial*i
-  print factorial 
+  print ("factorial") 
